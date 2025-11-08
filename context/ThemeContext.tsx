3a56c78db5a79565a92144225d40e8cd38fae0dd
Saba Phruidze <sabaphruidze01@gmail.com>
2025-11-08 21:27:42 +0400

    added usecontext and create context so for user's the app will be adjusted to the phone mode and than user could toggle it

---
+++ context/ThemeContext.tsx
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useState } from "react";
+import { useColorScheme } from "react-native";
+
+type Theme = "light" | "dark";
+
+interface ThemeContextData {
+  theme: Theme;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextData | undefined>(undefined);
+
+const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const colorScheme = useColorScheme() ?? "light"; // რა mode იც უყენია მომხმარებელს იმ სტილში წარმოაჩენს
+  const [theme, setTheme] = useState<Theme>(colorScheme);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (Object.is(prevTheme, "light") ? "dark" : "light"));
+  };
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (Object.is(context, undefined)) {
+    throw new Error(
+      "useTheme აუცილებლად უნდა გამოვიყენო ThemeProvider ის შიგნით"
+    );
+  }
+  return context;
+};

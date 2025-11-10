0fda185a338dfced79835f1b449eeecf64a4c847
Saba Phruidze <sabaphruidze01@gmail.com>
2025-11-10 21:51:32 +0400

    glich which I had on background after pressing input is solved . I used SafeAreaView, inside than TouchableWithoutFeedback than keyboard avoiding View and than scrollView, which solved the problem

--- app/login.tsx
+++ app/login.tsx
@@ -6,17 +6,26 @@ import FormButton from "@/components/auth/FormButton";
 import FormInput from "@/components/auth/FormInput";
 import ThemeToggle from "@/components/ui/ThemeToggle";
 import { useTheme } from "@/context/ThemeContext";
+import { useHeaderHeight } from "@react-navigation/elements";
 import { useRouter } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import {
   Alert,
+  Keyboard,
   KeyboardAvoidingView,
   Platform,
   ScrollView,
   StyleSheet,
   Text,
+  TouchableWithoutFeedback,
+  View,
 } from "react-native";
+
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 
 const LoginScreen = () => {
   const { theme, toggleTheme } = useTheme();
@@ -28,71 +37,105 @@ const LoginScreen = () => {
   const handleLogin = async () => {
     if (!email || !password) {
       Alert.alert("შეცდომა", "გთხოვთ შეავსეთ ყველა ველი");
-    }
-    setLoading(true);
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-      setLoading(false);
-      Alert.alert("ავტორიზაცია წარმატებით დასრულდა");
-      router.replace("/(tabs)");
-    } catch (e) {
-      setLoading(false);
-      Alert.alert("ავტორიზაციის შეცდომა", "იმეილი ან პაროლი არასწორია");
+    } else {
+      setLoading(true);
+      try {
+        await signInWithEmailAndPassword(auth, email, password);
+        setLoading(false);
+        Alert.alert("ავტორიზაცია წარმატებით დასრულდა");
+        router.replace("/(tabs)");
+      } catch (e: any) {
+        setLoading(false);
+        Alert.alert("ავტორიზაციის შეცდომა", "იმეილი ან პაროლი არასწორია");
+        console.log(e.name, e.message, e.stack);
+      }
     }
   };
+
+  const insets = useSafeAreaInsets();
+  const headerHeight = useHeaderHeight();
+  const keyboardVerticalOffset =
+    Platform.OS === "ios" ? headerHeight + insets.top : 0;
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={Object.is(theme, "light") ? styles.kavLight : styles.kavDark}
+    <SafeAreaView
+      style={Object.is(theme, "light") ? styles.safeLight : styles.safeDark}
+      edges={["top", "right", "left", "bottom"]}
     >
       <StatusBar style={Object.is(theme, "light") ? "dark" : "light"} />
-      <ThemeToggle />
-      <ScrollView
-        style={{ flex: 1 }}
-        contentContainerStyle={
-          Object.is(theme, "light")
-            ? styles.containerLight
-            : styles.containerDark
-        }
-        keyboardShouldPersistTaps="handled"
+
+      <TouchableWithoutFeedback
+        onPress={() => Keyboard.dismiss}
+        accessible={false}
       >
-        <Text
-          style={
-            Object.is(theme, "light") ? styles.titleLight : styles.titleDark
-          }
-        >
-          ავტორიზაცია
-        </Text>
-        <FormInput
-          placeholder="მეილი"
-          onChangeText={setEmail}
-          value={email}
-          keyboardType="email-address"
-          autoCapitalize="none"
-        />
-        <FormInput
-          placeholder="პაროლი"
-          onChangeText={setPassword}
-          value={password}
-          secureTextEntry
-        />
-        <FormButton
-          onPress={handleLogin}
-          loading={loading}
-          title={loading ? "მოწმდება თქვენი მონაცემები ... " : "შესვლა"}
-        />
-        <AuthLink
-          onPress={() => router.push("/register")}
-          linkText="თუ არ გაქვთ ანგარიში შექმნილი გადადი ამ გვერდზე დასარეგისტრირებლად "
-        />
-      </ScrollView>
-    </KeyboardAvoidingView>
+        <View style={styles.flex}>
+          <KeyboardAvoidingView
+            style={styles.flex}
+            behavior={Object.is(Platform.OS, "ios") ? "padding" : "height"}
+            keyboardVerticalOffset={keyboardVerticalOffset}
+          >
+            <ScrollView
+              style={{ flex: 1 }}
+              contentContainerStyle={
+                Object.is(theme, "light")
+                  ? styles.containerLight
+                  : styles.containerDark
+              }
+              keyboardShouldPersistTaps="handled"
+            >
+              <ThemeToggle />
+              <Text
+                style={
+                  Object.is(theme, "light")
+                    ? styles.titleLight
+                    : styles.titleDark
+                }
+              >
+                ავტორიზაცია
+              </Text>
+              <FormInput
+                placeholder="მეილი"
+                onChangeText={setEmail}
+                value={email}
+                keyboardType="email-address"
+                autoCapitalize="none"
+              />
+              <FormInput
+                placeholder="პაროლი"
+                onChangeText={setPassword}
+                value={password}
+                secureTextEntry
+              />
+              <FormButton
+                onPress={handleLogin}
+                loading={loading}
+                title={loading ? "მოწმდება თქვენი მონაცემები ... " : "შესვლა"}
+              />
+              <AuthLink
+                onPress={() => router.push("/register")}
+                linkText="თუ არ გაქვთ ანგარიში შექმნილი გადადი ამ გვერდზე დასარეგისტრირებლად "
+              />
+            </ScrollView>
+          </KeyboardAvoidingView>
+        </View>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
   );
 };
 
 export default LoginScreen;
 
 const styles = StyleSheet.create({
+  safeLight: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
+  },
+  safeDark: {
+    flex: 1,
+    backgroundColor: "#121212",
+  },
+  flex: {
+    flex: 1,
+  },
   kavLight: {
     flex: 1,
     backgroundColor: "#F5F5F5",

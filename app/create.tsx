da97165f0bc1499bf3fe439181a8a5c292cd117f
Saba Phruidze <sabaphruidze01@gmail.com>
2025-11-15 21:26:44 +0400

    Added safeAreaView, TouchableWithout Feedback, View and Keyboard avoiding View so the keyboard will not hide the input

--- app/create.tsx
+++ app/create.tsx
@@ -2,15 +2,28 @@ import FormInput from "@/components/auth/FormInput";
 import { useTheme } from "@/context/ThemeContext";
 import React, { useState } from "react";
 import {
+  ActivityIndicator,
   Alert,
   Image,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
   Pressable,
   ScrollView,
   StyleSheet,
   Text,
+  TouchableWithoutFeedback,
+  View,
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import * as ImagePicker from "expo-image-picker";
+import * as Location from "expo-location";
+
+interface SpotLocation {
+  latitude: number;
+  longitude: number;
+}
 
 export default function CreateSpotScreen() {
   const { theme } = useTheme();
@@ -18,6 +31,8 @@ export default function CreateSpotScreen() {
   const [title, setTitle] = useState<string>("");
   const [description, setDescription] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
+  const [locationLoading, setLocationLoading] = useState<boolean>(false);
+  const [location, setLocation] = useState<SpotLocation | null>(null);
 
   const pickImage = async () => {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -36,59 +51,148 @@ export default function CreateSpotScreen() {
       setImageUri(result.assets[0].uri);
     }
   };
+  const getLocation = async () => {
+    setLocationLoading(true);
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      Alert.alert("ნებართვა საჭიროა", "გთხოვთ, დაუშვათ წვდომა ლოცაკიაზე");
+      setLocationLoading(false);
+      return;
+    }
+    try {
+      const locationResult = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.High,
+      });
+      setLocation({
+        latitude: locationResult.coords.latitude,
+        longitude: locationResult.coords.longitude,
+      });
+    } catch (e) {
+      Alert.alert("შეცდომა", "ლოცაკიის დამახსოვრება ვერ მოხერხდა");
+    }
+    setLocationLoading(false);
+  };
 
   const handleSubmit = async () => {
-    if (!imageUri || !title || !description) {
+    if (!imageUri || !title || !description || !location) {
       Alert.alert("შეცდომა", "შეავსეთ ყველა ველი და აირჩიე სურათი");
       return;
     }
     setLoading(true);
-    console.log("Submitting:", { imageUri, title, description });
-
+    console.log("Submitting:", { imageUri, title, description, location });
     setLoading(false);
   };
   return (
-    <ScrollView
+    <SafeAreaView
       style={
         Object.is(theme, "light") ? styles.containerLight : styles.containerDark
       }
+      edges={["top", "right", "left", "bottom"]}
     >
-      <Text
-        style={Object.is(theme, "light") ? styles.titleLight : styles.titleDark}
+      <TouchableWithoutFeedback
+        onPress={() => Keyboard.dismiss}
+        accessible={false}
       >
-        დაამატე ახალი საუნჯე
-      </Text>
-      <Pressable style={styles.imagePickerButton} onPress={pickImage}>
-        <Text style={styles.imagePickerButtonText}>
-          {imageUri ? "სურათის შეცვლა" : "სურათის არჩევა"}
-        </Text>
-      </Pressable>
-      {imageUri && (
-        <Image source={{ uri: imageUri }} style={styles.previewImage} />
-      )}
-      <FormInput placeholder="სათაური" value={title} onChangeText={setTitle} />
-      <FormInput
-        placeholder="აღწერა"
-        numberOfLines={4} //საწყისი სიმაღლე რამხელა იყოს
-        value={description}
-        multiline //რამდენიმე ხაზიანი რომ იყოს
-        onChangeText={setDescription}
-        style={Object.is(theme, "light") ? styles.inputDark : styles.inputLight}
-      />
-      <Pressable
-        style={[styles.submitButton, loading && styles.submitButtonDisabled]}
-        onPress={handleSubmit}
-        disabled={loading}
-      >
-        <Text style={styles.submitButtonText}>
-          {loading ? "იტვირთება" : "საუნჯე დაემატა"}
-        </Text>
-      </Pressable>
-    </ScrollView>
+        <View style={styles.flex}>
+          <KeyboardAvoidingView
+            style={styles.flex}
+            behavior={Object.is(Platform.OS, "ios") ? "padding" : "height"}
+          >
+            <ScrollView
+              style={
+                Object.is(theme, "light")
+                  ? styles.containerLight
+                  : styles.containerDark
+              }
+              contentContainerStyle={{ paddingBottom: 100 }}
+            >
+              <Text
+                style={
+                  Object.is(theme, "light")
+                    ? styles.titleLight
+                    : styles.titleDark
+                }
+              >
+                დაამატე ახალი საუნჯე
+              </Text>
+              <Pressable style={styles.imagePickerButton} onPress={pickImage}>
+                <Text style={styles.imagePickerButtonText}>
+                  {imageUri ? "სურათის შეცვლა" : "სურათის არჩევა"}
+                </Text>
+              </Pressable>
+              {imageUri && (
+                <Image source={{ uri: imageUri }} style={styles.previewImage} />
+              )}
+              <Pressable
+                style={styles.locationButton}
+                onPress={getLocation}
+                disabled={locationLoading}
+              >
+                {locationLoading ? (
+                  <ActivityIndicator
+                    color={Object.is(theme, "light") ? "#fff" : "#000"}
+                    size={20}
+                  />
+                ) : (
+                  <Text style={styles.locationButtonText}>
+                    {location ? "ლოკაციის განახლება" : "ლოკაციის აღება"}
+                  </Text>
+                )}
+              </Pressable>
+
+              {location && (
+                <Text
+                  style={
+                    Object.is(theme, "light")
+                      ? styles.locationTextLight
+                      : styles.locationTextDark
+                  }
+                >
+                  Lat: {location.latitude.toFixed(4)}, Lon:{" "}
+                  {location.longitude.toFixed(4)}
+                </Text>
+              )}
+              <FormInput
+                placeholder="სათაური"
+                value={title}
+                onChangeText={setTitle}
+              />
+              <FormInput
+                placeholder="აღწერა"
+                numberOfLines={4} //საწყისი სიმაღლე რამხელა იყოს
+                value={description}
+                multiline //რამდენიმე ხაზიანი რომ იყოს
+                onChangeText={setDescription}
+                style={
+                  Object.is(theme, "light")
+                    ? styles.inputDark
+                    : styles.inputLight
+                }
+              />
+              <Pressable
+                style={[
+                  styles.submitButton,
+                  loading && styles.submitButtonDisabled,
+                ]}
+                onPress={handleSubmit}
+                disabled={loading}
+              >
+                <Text style={styles.submitButtonText}>
+                  {loading ? "იტვირთება" : "საუნჯე დაემატა"}
+                </Text>
+              </Pressable>
+            </ScrollView>
+          </KeyboardAvoidingView>
+        </View>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
   containerLight: {
     flex: 1,
     backgroundColor: "#F5F5F5",
@@ -172,5 +276,29 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: "#333",
     textAlignVertical: "top",
+  },
+  locationButton: {
+    backgroundColor: "#D2691E",
+    padding: 15,
+    borderRadius: 8,
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  locationButtonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  locationTextLight: {
+    color: "#333",
+    textAlign: "center",
+    marginBottom: 20,
+    fontSize: 14,
+  },
+  locationTextDark: {
+    color: "#CCC",
+    textAlign: "center",
+    marginBottom: 20,
+    fontSize: 14,
   },
 });

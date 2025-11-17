e12f2bd992b13af99dc07e54b10495d0a485f966
Saba Phruidze <sabaphruidze01@gmail.com>
2025-11-17 20:11:49 +0400

    The issue I undestood . The problem was that I through I have to pay but google just wanted me to add my card and even if I not pay it will not be a problem . currently things work well. image, registration and other things

--- firebaseConfing.js
+++ firebaseConfing.js
@@ -10,7 +10,6 @@ const firebaseConfig = {
   storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.EXPO_PUBLIC_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID,
 };
 if (!firebaseConfig.apiKey) {
   throw new Error("Firebase API_ს გასაღები აკლია.  შეამოწმე .env ფაილი");

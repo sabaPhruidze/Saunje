ef619200b48a31275abbe208277c603cfb8400ce
Saba Phruidze <sabaphruidze01@gmail.com>
2025-11-19 21:41:43 +0400

    Added new folder of spots. the goal is to make refactoring

---
+++ components/spots/SpotCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const SpotCard = () => {
+  return (
+    <View>
+      <Text>SpotCard</Text>
+    </View>
+  );
+};
+
+export default SpotCard;

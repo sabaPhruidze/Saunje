b2eb68ff3930c403e61d62cdcc8278260b6ac477
Saba Phruidze <sabaphruidze01@gmail.com>
2025-11-21 21:44:38 +0400

    have an issue with the button . it must display as an absolute but the icon does not move

--- app/map.tsx
+++ app/map.tsx
@@ -1,7 +1,43 @@
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 import React from "react";
+import { StyleSheet, View } from "react-native";
 
 function MapScreen() {
-  return <div>MapScreen</div>;
+  const router = useRouter();
+  return (
+    <View style={styles.conatiner}>
+      <Ionicons
+        name="close-circle"
+        size={40}
+        color="rgba(0,0,0,0.6)"
+        onPress={() => router.back()}
+      />
+    </View>
+  );
 }
 
 export default MapScreen;
+
+const styles = StyleSheet.create({
+  conatiner: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  closeButton: {
+    position: "absolute",
+    top: 40,
+    left: 16,
+    backgroundColor: "rgba(0,0,0,0.5)",
+    borderRadius: 20,
+    zIndex: 1,
+  },
+});

2bd1f0f020bc9487548eefdcf2cee32973d8d324
Saba Phruidze <sabaphruidze01@gmail.com>
2025-11-21 22:00:21 +0400

    So I imported database , collection and getDocs and made a logic inside useEffect async/await + try/catch/finally . I never through finally would be necessary but this proved that we could remove loading by using it

--- app/map.tsx
+++ app/map.tsx
@@ -1,10 +1,44 @@
+import { db } from "@/firebaseConfing";
 import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { Alert, StyleSheet, View } from "react-native";
+
+interface SpotLocation {
+  latitude: number;
+  longitude: number;
+}
+
+interface Spot {
+  id: string;
+  title: string;
+  description: string;
+  location: SpotLocation;
+}
 
 function MapScreen() {
   const router = useRouter();
+  const [spots, setSpots] = useState<Spot[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchSpots = async () => {
+      try {
+        const querySnapShoot = await getDocs(collection(db, "Saunje"));
+        const spotsData = querySnapShoot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        })) as Spot[];
+        setSpots(spotsData);
+      } catch (e) {
+        Alert.alert("შეცდომა", "backend სერვერზე წვდომა შეზღუდულია");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchSpots();
+  }, []);
   return (
     <View style={styles.conatiner}>
       <Ionicons

2b86d424706598c7e912268339748001a8cdbdcb
Saba Phruidze <sabaphruidze01@gmail.com>
2025-12-10 19:54:05 +0400

    this time it should work

--- firebaseConfing.js
+++ firebaseConfing.js
@@ -1,3 +1,13 @@
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { getApp, getApps, initializeApp } from "firebase/app";
+import {
+  getAuth,
+  getReactNativePersistence,
+  initializeAuth,
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
@@ -7,22 +17,33 @@ const firebaseConfig = {
   appId: process.env.EXPO_PUBLIC_APP_ID,
 };
 
+console.log("FIREBASE CONFIG IN JS:", firebaseConfig);
+
 if (!firebaseConfig.apiKey) {
   if (__DEV__) {
-    // development-ში უბრალოდ შეგვაჩეროს, რომ შეცდომა დავინახოთ
     throw new Error("Firebase API_ს გასაღები აკლია.  შეამოწმე .env ფაილი");
   } else {
-    // production-ში აპი აღარ უნდა დაექრაშოს მხოლოდ ამის გამო
     console.log("Firebase API key is missing in production build");
   }
 }
 
-export const app = initializeApp(firebaseConfig);
+// app – ინიციალიზდება მხოლოდ ერთხელ
+export const app =
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
+// აქ ვზრუნავთ, რომ auth მხოლოდ ერთხელ შეიქმნას persistence-ით
+let authInstance;
+
+if (getApps().length === 1) {
+  // პირველად ვქნათ initializeAuth
+  authInstance = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+  });
+} else {
+  // თუ უკვე არის app, auth-ს უბრალოდ getAuth-ით ვიღებთ
+  authInstance = getAuth(app);
+}
+
+export const auth = authInstance;
 export const db = getFirestore(app);
-const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
-});
-
-export { auth };
 export const storage = getStorage(app);
